47e441bc24213f919ab4cb3241db3593fed9fa1a
tsousacorreia <tsousacorreia@outlook.com>
2024-04-15 19:30:29 -0300

    Atualizado 15-04-2024

--- atividade7.php
+++ atividade7.php
@@ -9,16 +9,255 @@
 	$totalLugares = 240;
 
 	$mesa = 0;
-	$lugares = 6;
+	$lugares = 0;
 
-	for ($i=1; $i < 40; $i++) { 
-		if ($mesa !== 0) {
+
+	switch ($mesa > 0) {
+		case '1':
 			$totalMesas--;
 			$totalLugares = $totalLugares - $lugares;
-		}else{
-			$i = 0;
-		}
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '2':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '3':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;		
+
+		case '4':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '5':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '6':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '7':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '8':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '9':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '10':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '11':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '12':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '13':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;		
+
+		case '14':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '15':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '16':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '17':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '18':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '19':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '20':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '21':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '22':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '23':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;		
+
+		case '24':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '25':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '26':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '27':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '28':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '29':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '30':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '31':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '32':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '33':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;		
+
+		case '34':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '35':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '36':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '37':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '38':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		case '39':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;			
+
+		case '40':
+			$totalMesas--;
+			$totalLugares = $totalLugares - $lugares;
+			echo "Dados da reserva:<br \>Mesa nº$mesa<br \>Lugares: $lugares<br \><br \>";
+			break;
+
+		default:
+			echo "Reserva não realizada<br \>";
+			break;
 	}
 
 	echo "<br \> Ainda há $totalMesas mesas disponíveis e $totalLugares lugares.";
+
 ?>
