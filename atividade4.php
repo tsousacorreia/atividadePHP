0409276bf6110981f858249c59d4caaf6a2ebc8a
tsousacorreia <tsousacorreia@outlook.com>
2024-04-15 16:20:43 -0300

    atualizado 15-04-2024

--- atividade4.php
+++ atividade4.php
@@ -3,40 +3,40 @@
 	//Atividade número 4
 	//Dupla: Thiago de Sousa Correia / Silvio Moiano Silva
 
-	$num1 = 10;
-	$num2 = 12;
-	$num3 = 20;
+	$num1 = 3;
+	$num2 = 2;
+	$num3 = 1;
 
-	if($num1 > $num2){
-		if($num2 > $num3){
-			echo "$num1 <br />";
-			echo "$num2 <br />";
-			echo "$num3 <br />";	
-		}else{
-			echo "$num1 <br />";
-			echo "$num3 <br />";
-			echo "$num2 <br />";
-		}
-	}else if($num2 > $num3){
-		if($num3 > $num1){
-			echo "$num2 <br />";
-			echo "$num3 <br />";
-			echo "$num1 <br />";
-		}else{
-			echo "$num2 <br />";
-			echo "$num1 <br />";
-			echo "$num3 <br />";
-		}
-	}else if($num3 > $num1){
-		if($num1 > $num2){
-			echo "$num3 <br />";
-			echo "$num1 <br />";
-			echo "$num2 <br />";
-		}else{
-			echo "$num3 <br />";
-			echo "$num2 <br />";
-			echo "$num1 <br />";
-		}
+	if ($num1 >= $num2 && $num1 >= $num3) {
+	    if ($num2 >= $num3) {
+	        echo "$num1 <br />";
+	        echo "$num2 <br />";
+	        echo "$num3 <br />";
+	    } else {
+	        echo "$num1 <br />";
+	        echo "$num3 <br />";
+	        echo "$num2 <br />";
+	    }
+	} elseif ($num2 >= $num1 && $num2 >= $num3) {
+	    if ($num1 >= $num3) {
+	        echo "$num2 <br />";
+	        echo "$num1 <br />";
+	        echo "$num3 <br />";
+	    } else {
+	        echo "$num2 <br />";
+	        echo "$num3 <br />";
+	        echo "$num1 <br />";
+	    }
+	} elseif ($num3 >= $num1 && $num3 >= $num2) {
+	    if ($num1 >= $num2) {
+	        echo "$num3 <br />";
+	        echo "$num1 <br />";
+	        echo "$num2 <br />";
+	    } else {
+	        echo "$num3 <br />";
+	        echo "$num2 <br />";
+	        echo "$num1 <br />";
+	    }
 	}
 	
  ?>
